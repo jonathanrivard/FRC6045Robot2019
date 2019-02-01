3ccab66167adf5760df8687043aa6f49bad0d037
John Rivard <jrivard23@outlook.com>
2019-02-01 12:10:44 -0600

    Commented Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,52 +11,30 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   public static OI m_oi;
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+  //This function is run when the robot starts up
   @Override
   public void robotInit() {
     m_oi = new OI();
-
-
     //m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     //SmartDashboard.putData("Auto mode", m_chooser);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+  //This function is called every robot packet, no matter the mode
+  //Use this for stuff like diagnostics 
   @Override
   public void robotPeriodic() {
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
+  //This function is called each time the robot enter Disabled mode
   @Override
   public void disabledInit() {
   }
@@ -94,9 +72,7 @@ public class Robot extends TimedRobot {
     } */
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+  //This function is called periodically during autonomous.
   @Override
   public void autonomousPeriodic() {
     Scheduler.getInstance().run();
@@ -113,17 +89,13 @@ public class Robot extends TimedRobot {
     } */
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
+  //This function is called periodically during operator control.
   @Override
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
   }
-
-  /**
-   * This function is called periodically during test mode.
-   */
+  
+  //This function is called periodically during test mode.
   @Override
   public void testPeriodic() {
   }
