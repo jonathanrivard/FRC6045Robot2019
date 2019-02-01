dfa3eb4c9e265bbd6b3d0156bd1ecb7c2b31f24c
Cameron Huse <camjams101@yahoo.com>
2019-02-01 12:00:35 -0600

    BUTTONS

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,4 +2,7 @@ package frc.robot;
 
 public class RobotMap {
   
+    public static int JOYSTICK_USB = 0;
+    public static int JOYSTICK_BUTTON1 = 1;
+    public static int JOYSTICK_BUTTON2 = 2;
 }
