6405ce8a5c8c74018ff4b83b5bb61f28ad5c567e
Cameron Huse <camjams101@yahoo.com>
2019-02-11 17:23:20 -0600

    Completed Grabtoposition?

--- src/main/java/frc/robot/commands/GrabToPosition.java
+++ src/main/java/frc/robot/commands/GrabToPosition.java
@@ -9,6 +9,7 @@ package frc.robot.commands;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
@@ -16,12 +17,19 @@ import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
 public class GrabToPosition extends Command {
-   
-  public GrabToPosition() {
+   long ticks;
+  public GrabToPosition(int position) {
     requires(Robot.m_claw);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-  }
+    switch(position){
+      case 1: ticks = RobotMap.CLAW_OPEN_POSITION;
+      break;
+      case 2: ticks = RobotMap.CLAW_CLOSED_POSITION;
+      break;
+    }
+   
+}
 
   // Called just before this Command runs the first time
   @Override
@@ -33,7 +41,21 @@ public class GrabToPosition extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    int threshold = 1000;
+    double clawSpeed = 0.1;
+    long current = Robot.m_claw.getClawMotor().getSelectedSensorPosition();
+    long error = ticks - current;
+
+    if(error > threshold){
+      Robot.m_lift.setPercentage(clawSpeed);
+    }else if (error < -1 * threshold){
+      Robot.m_lift.setPercentage(clawSpeed);
+    }else {
+      Robot.m_lift.setPercentage(0);
+    }
   }
+
+  
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
