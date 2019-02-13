26574e61c040d28404b4d0520b534613acf53083
Cameron Huse <camjams101@yahoo.com>
2019-02-13 16:46:20 -0600

    got rid of robot map... import

--- src/main/java/frc/robot/commands/ManualGrab.java
+++ src/main/java/frc/robot/commands/ManualGrab.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.RobotMap;
+
 
 public class ManualGrab extends Command {
   public ManualGrab() {
