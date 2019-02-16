57914ea4eb26cdddd21d6d55681b5f3d7227383f
John Rivard <jrivard23@outlook.com>
2019-02-16 10:13:40 -0600

    Tilt is now a Spark

--- src/main/java/frc/robot/subsystems/ClawTilter.java
+++ src/main/java/frc/robot/subsystems/ClawTilter.java
@@ -11,29 +11,28 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
 public class ClawTilter extends Subsystem {
   //INTANCE VARIABLES 
   //-Motors
-  TalonSRX tiltMotor;
+  Spark tiltMotor;
   //-Limit Switches
   DigitalInput topLimit;
   DigitalInput bottomLimit;
   //GETTERS
   //-Motors
-  public TalonSRX getTiltMotor() { return tiltMotor; }
+  public Spark getTiltMotor() { return tiltMotor; }
   //-Limit Switches
   public boolean getTopLimit() { return topLimit.get(); }
   public boolean getBottomLimit() { return bottomLimit.get(); }
 
   public ClawTilter(){
     //Setup tiler claw
-    tiltMotor = new TalonSRX(RobotMap.MOTOR_CLAW_TILT);
+    tiltMotor = new Spark(RobotMap.MOTOR_CLAW_TILT);
     tiltMotor.setInverted(false);
-    tiltMotor.configPeakCurrentDuration(0, 30);
-    tiltMotor.setSelectedSensorPosition(0);
     //Limit Switches
     topLimit = new DigitalInput(RobotMap.DIGITAL_CLAW_TILTER_TOP);
     bottomLimit = new DigitalInput(RobotMap.DIGITAL_CLAW_TILTER_BOTTOM);
@@ -41,7 +40,7 @@ public class ClawTilter extends Subsystem {
 
   //Set speed of tilt motor
   public void setPercentage(double per){
-    tiltMotor.set(ControlMode.PercentOutput, per);
+    tiltMotor.set(per);
   }
 
   @Override
