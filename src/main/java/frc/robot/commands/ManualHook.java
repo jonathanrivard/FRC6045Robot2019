3da670d6fcadc4bb9153921f00819c306c6f4771
John Rivard <jrivard23@outlook.com>
2019-02-18 15:29:10 -0600

    Maunal Hook

--- src/main/java/frc/robot/commands/ManualHook.java
+++ src/main/java/frc/robot/commands/ManualHook.java
@@ -32,7 +32,7 @@ public class ManualHook extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return false; 
   }
 
   // Called once after isFinished returns true
